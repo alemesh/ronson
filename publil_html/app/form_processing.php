8f9e51afd743e3ab437043c7eef5884fba42a9da
alemesh <alemesh@acceptic.com>
2018-05-02 18:46:39 +0300

    added ref sistem

--- publil_html/app/form_processing.php
+++ publil_html/app/form_processing.php
@@ -4,12 +4,27 @@
 $senderName = 'Ronson';
 $senderEmail = $_SERVER['SERVER_NAME'];
 $targetEmail = [];
-$targetEmail = ['ravit@gofmans.co.il', 'office@ronson.co.il', 'idan@ronson.co.il', 'alemesh@acceptic.com', 'sales1@ronson.co.il'];
+$targetEmail = ['ravit@gofmans.co.il', 'office@ronson.co.il', 'idan@ronson.co.il', 'alemesh@acceptic.com', 'sales1@ronson.co.il', 'eli@gofmans.co.il'];
 //$targetEmail = ['alemesh@acceptic.com'];
 $messageSubject = 'Message from web-site - '. $_SERVER['SERVER_NAME'];
 $redirectToReferer = true;
 $redirectURL = $_SERVER['SERVER_NAME'];
 //****************************************
+
+//serchh Media title =========
+$mystr = $_SERVER['HTTP_REFERER'];
+$mystrarr = explode('?', $mystr);
+$findname = 'ck=';
+foreach ($mystrarr as $value){
+    $pos = strpos($value, $findname);
+    if($pos !== false){
+        $posMediaTitle = explode('=', $value);
+        $MediaTitle = $posMediaTitle[1];
+    }
+}
+//============================
+
+
 
 // mail content
 $ufname = $_POST['name'];
@@ -17,11 +32,13 @@ $uphone = $_POST['tel'];
 $umail = $_POST['email'];
 $ProjectID = $_POST['ProjectID'];
 $Password = $_POST['Password'];
+$reffererName = $_SERVER['HTTP_REFERER'];
 
 // prepare message text
 $messageText =	'First Name: '.$ufname."\n".
     'Phone: '.$uphone."\n".
-    'Email: '.$umail."\n";
+    'Email: '.$umail."\n".
+    'Referal: '.$reffererName."\n";
 
 // send email
 $senderName = "=?UTF-8?B?" . base64_encode($senderName) . "?=";
@@ -42,11 +59,13 @@ $url = 'http://www.bmby.com/shared/AddClient/index.php';
 //$url = 'http://192.168.89.147/test.php';
 //$url = 'http://testbmby/test.php';
 $params = array(
-    'Fname' => $ufname, // в http://localhost/post.php это будет $_POST['param1'] == '123'
-    'Phone' => $uphone, // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
-    'Email' => $umail, // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
-    'ProjectID' => $ProjectID, // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
-    'Password' => $Password // в http://localhost/post.php это будет $_POST['param2'] == 'abc'
+    'Fname' => $ufname,
+    'Phone' => $uphone,
+    'Email' => $umail,
+    'ProjectID' => $ProjectID,
+    'Password' => $Password,
+    'Referal' => $reffererName,
+    'MediaTitle' => $MediaTitle
 );
 $result = file_get_contents($url, false, stream_context_create(array(
     'http' => array(
@@ -92,7 +111,7 @@ $result = file_get_contents($url, false, stream_context_create(array(
 
 
 $urlredirect = 'http://www.ronson.co.il/thanks-page.html?Lead=true';
-//$urlredirect = 'http://192.168.89.147/';
+//$urlredirect = 'http://192.168.89.147/thanks-page.html?Lead=true';
 //// redirect
 if($redirectToReferer) {
     header("Location: ".$urlredirect);
